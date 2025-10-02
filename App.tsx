d7fc7fcaf0b62e378bceb95c39b9bb244bc9d194
bogidemasrepo <bogidemas@gmail.com>
2025-10-02 04:52:39 +0300

    navigation added

--- App.tsx
+++ App.tsx
@@ -1,28 +1,47 @@
 import { NavigationContainer } from '@react-navigation/native';
 import {createNativeStackNavigator} from "@react-navigation/native-stack"
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import IndexScreen from './screens/IndexScreen';
+import AuthScreen from './screens/AuthScreen';
+import LoginScreen from './screens/LoginScreen';
+import SignupScreen from './screens/SignupScreen';
+import VerifyOtp from './screens/VerifyOtp';
+import ForgotPassword from './screens/ForgotPassword';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from './screens/HomeScreen';
+import ProfileScreen from './screens/ProfileScreen';
+import OrdersScreen from './screens/OrdersScreen';
+import CartScreen from './screens/CartScreen';
 
-function App() {
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function Tabs() {
   return (
-    <View style={styles.container}>
-      <Text>FoodGo!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+       <Tab.Screen name="Cart" component={CartScreen} />
+       <Tab.Screen name="Orders" component={OrdersScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+    </Tab.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
 export default ()=>{
-  return <NavigationContainer>
-      <App />
-  </NavigationContainer>
+
+  return  <SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+          <Stack.Screen name='index' component={()=><IndexScreen />} />
+          <Stack.Screen name='auth' component={()=><AuthScreen />} />
+          <Stack.Screen name='login' component={()=><LoginScreen />} />
+          <Stack.Screen name='signup' component={()=><SignupScreen />} />
+          <Stack.Screen name='verifyOtp' component={()=><VerifyOtp />} />
+          <Stack.Screen name='forgotPassword' component={()=><ForgotPassword />} />
+          <Stack.Screen name='Tabs' component={Tabs} />
+      </Stack.Navigator>
+    </NavigationContainer>
+    <StatusBar style="auto" />
+  </SafeAreaView>
 }
